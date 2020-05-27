4607d6bf397c2a5b2efe76b695c27d82a9a09a18
pichirina-dev <pichirina@me.com>
2020-05-27 19:17:40 +0300

    added sliders

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,33 @@
 $(function(){
 
+    $(".product__item-star").rateYo({
+        rating: 4.5,
+        readOnly: true,
+        starWidth: "12px",
+        ratedFill: "#ffc000",
+        spacing: "5px"
+    });
+
+    $('.product-slider-inner').slick({
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        centerMode: true,
+        appendArrows: ".product-slider__arrows",
+        prevArrow: '<button class="product-slider-arrow--left">',
+        nextArrow: '<button class="product-slider-arrow--right">'
+    });
+
+    $('.weekly__product-slider-inner').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        appendArrows: ".weekly__product-slider__arrows",
+        prevArrow: '<button class="product-slider-arrow--left">',
+        nextArrow: '<button class="product-slider-arrow--right">'
+     
+    });
+
    var mixer = mixitup('.products__inner-box');
     
 });
+
+
