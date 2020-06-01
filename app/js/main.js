4a488ccd2c583c632846172b4475037994d9d742
pichirina-dev <pichirina@me.com>
2020-06-01 19:39:15 +0300

    added new items

--- app/js/main.js
+++ app/js/main.js
@@ -26,6 +26,16 @@ $(function(){
      
     });
 
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 200,
+        to: 500,
+        grid: true
+    });
+    
+
    var mixer = mixitup('.products__inner-box');
     
 });
