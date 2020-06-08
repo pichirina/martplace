d1f8e590a81159defe38b37cc5dfb9ae97190904
pichirina-dev <pichirina@me.com>
2020-06-08 21:06:55 +0300

    added new items + list

--- app/js/main.js
+++ app/js/main.js
@@ -8,6 +8,22 @@ $(function(){
         spacing: "5px"
     });
 
+    $(".product-page__item-stars").rateYo({
+        rating: 4.5,
+        readOnly: true,
+        starWidth: "12px",
+        ratedFill: "#ffc000",
+        spacing: "5px"
+    });
+
+    $(".product-page__item--list-stars").rateYo({
+        rating: 4.5,
+        readOnly: true,
+        starWidth: "12px",
+        ratedFill: "#ffc000",
+        spacing: "5px"
+    });
+
     $('.product-slider-inner').slick({
         slidesToShow: 3,
         slidesToScroll: 2,
@@ -33,6 +49,20 @@ $(function(){
         from: 30,
         to: 300,
         prefix: "$"
+    });
+
+    $('.page-filter-list__btn').on('click',function(){
+        $('.product-page__item').addClass('list');
+        $('.page-filter-list__btn').addClass('active');
+        $('.page-filter-grid__btn').removeClass('active');
+        
+
+    });
+
+    $('.page-filter-grid__btn').on('click',function(){
+        $('.product-page__item').removeClass('list');
+        $('.page-filter-grid__btn').addClass('active');
+        $('.page-filter-list__btn').removeClass('active');
     });
     
 
